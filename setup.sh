ca19d1e5990afeeb011e2046a32067d137dfee99
BuXaLo <buxalo666@hotmail.com>
2025-08-04 17:58:41 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "== Загрузка и запуск secure-proxy-setup.sh =="
+
+SCRIPT_URL="https://raw.githubusercontent.com/BuXaLo/secure-ubuntu-socks5/refs/heads/main/secure-proxy-setup.sh"
+SCRIPT_NAME="secure-proxy-setup.sh"
+
+# Скачиваем
+curl -sSL "$SCRIPT_URL" -o "$SCRIPT_NAME"
+
+# Делаем исполняемым
+chmod +x "$SCRIPT_NAME"
+
+# Запускаем
+sudo ./"$SCRIPT_NAME"
